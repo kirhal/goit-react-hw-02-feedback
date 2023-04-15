0df3abc3746c04de821bd00468f34c28a268757d
kirhal <kirillgaleckiy@gmail.com>
2023-04-15 22:39:32 +0200

    upd

--- src/components/feedbacks/Notification.jsx
+++ src/components/feedbacks/Notification.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import css from './Feedback.module.css';
 
 export function Notification({ message }) {
   return <p className={css[`statistics-item`]}>{message}</p>;
--- src/components/feedbacks/Section.jsx
+++ src/components/feedbacks/Section.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import css from './Feedback.module.css';
 
 export function Section({ title, children }) {
   return (
