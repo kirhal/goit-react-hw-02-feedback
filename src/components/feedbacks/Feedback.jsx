b20f1785f14a011d3d5c510091c612f8b2d8a07f
kirhal <kirillgaleckiy@gmail.com>
2023-04-15 21:44:57 +0200

    upd

--- src/components/feedbacks/Feedback.jsx
+++ src/components/feedbacks/Feedback.jsx
@@ -4,20 +4,22 @@ import css from './Feedback.module.css';
 
 export class Feedback extends Component {
   state = {
-    good: 5,
-    neutral: 3,
-    bad: 1,
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
 
-  // constructor() {
-  //   super();
-  //   this.countTotalFeedback = this.countTotalFeedback.bind(this.state);
-  // }
+  addFeedback = evt => {
+    const name = evt.target.name;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
 
-  countTotalFeedback(state) {
+  countTotalFeedback = state => {
     const valuesArr = Object.values(state);
     return valuesArr.reduce((previousValue, elem) => previousValue + elem, 0);
-  }
+  };
   countPositiveFeedbackPercentage(data) {
     return Math.round(data * 100);
   }
@@ -26,35 +28,50 @@ export class Feedback extends Component {
     const state = this.state;
     const totalFeedback = this.countTotalFeedback(state);
     const count = state.good / totalFeedback;
-    const feedbackPercentage = this.countPositiveFeedbackPercentage(count);
+    const positivePercentage = this.countPositiveFeedbackPercentage(count);
 
     return (
       <div className="container">
         <h2 className="title">Please leave feedback</h2>
         <ul className={css['buttons-list']}>
           <li>
-            <button type="button" className={css.button} name="good">
+            <button
+              type="button"
+              className={css.button}
+              name="good"
+              onClick={this.addFeedback}
+            >
               Good
             </button>
           </li>
           <li>
-            <button type="button" className={css.button} name="neutral">
+            <button
+              type="button"
+              className={css.button}
+              name="neutral"
+              onClick={this.addFeedback}
+            >
               Neutral
             </button>
           </li>
           <li>
-            <button type="button" className={css.button} name="bad">
+            <button
+              type="button"
+              className={css.button}
+              name="bad"
+              onClick={this.addFeedback}
+            >
               Bad
             </button>
           </li>
         </ul>
         <h2 className="title">Statistics</h2>
-        <p className="statistics-item">Good: {this.state.good}</p>
-        <p className="statistics-item">Neutral: {this.state.neutral}</p>
-        <p className="statistics-item">Bad: {this.state.bad}</p>
+        <p className="statistics-item">Good: {state.good}</p>
+        <p className="statistics-item">Neutral: {state.neutral}</p>
+        <p className="statistics-item">Bad: {state.bad}</p>
         <p className="statistics-item">Total: {totalFeedback}</p>
         <p className="statistics-item">
-          Positive feedback: {feedbackPercentage}%
+          Positive feedback: {positivePercentage}%
         </p>
       </div>
     );
